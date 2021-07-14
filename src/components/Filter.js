7da2ffbe2f4d07029fc4f7fc284b3e500de9f2d8
Oleg <roxvemm2014@gmail.com>
2021-07-13 19:43:33 -0700

    initial commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+// Filter is a child to ShoppingList 
+
+function Filter({ search, onSearchChange, onCategoryChange }) {
+
+// define function to handle input
+// takes event and whatever is inside of that even as an argument after any change 
+// passes the actual value typed in the search field to useState in the parent component
+  function handleSearchChange(event) {
+    // accessing the actual value in the search field & passing to the useSate 
+    onSearchChange(event.target.value);
+  }
+
   return (
+// create search field 
+// define value attribute, add onChange event Listener -> detects any changes in the search field and call handleSearchChange function 
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." value={search} onChange={handleSearchChange}/>
+
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
